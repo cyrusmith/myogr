89c1ae89885909f48dbffc3fda3c27f5f489cab4
Nick Lopin <lopinopulos@gmail.com>
2012-11-28 10:09:38 +0600

    User confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
