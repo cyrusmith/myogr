cc4836654119a9d40f8491b09a07565a93ae39bc
Nick Lopin <lopinopulos@gmail.com>
2012-12-04 23:13:06 +0600

    Routes fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
-  # :token_authenticatable,
+  # :token_authenticatable,      , :confirmable
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
