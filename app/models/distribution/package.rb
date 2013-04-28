717234c0a771f75757a98879c6db31bc86b6d44a
Semyannikov <r>
2013-04-28 20:22:50 +0600

    Models update

--- app/models/distribution/package.rb
+++ app/models/distribution/package.rb
@@ -6,5 +6,8 @@ class Distribution::Package
   field :order, type: Integer
   field :state, type: String
 
+  embeds_many :distribution_distributor
+  has_one :user
+
   belongs_to :package_list, class_name: 'Distribution::PackageList', inverse_of: :packages
 end
