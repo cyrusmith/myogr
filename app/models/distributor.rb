9eab068a96a15ef0b01c00819b8b72b88ff4a741
Nick Lopin <lopinopulos@gmail.com>
2013-06-05 01:42:31 +0600

    refs #169 Group by distributor added to method in_distribution_for_user refs #170 Item Id field added to table

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -12,6 +12,7 @@ class Distributor < ForumModels
         .where('color = 5')
         .where(ProductOrder.table_name => {member_id: user_id, show_buyer: 1})
         .where('ibf_zakup.status NOT IN (-2, 2)')
+        .group(self.primary_key)
   end
 
   def user_participate? (user_id)
