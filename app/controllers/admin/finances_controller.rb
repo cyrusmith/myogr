b2d4f19c1bb2be89efc6f8ad4ed8cc99120bfb6f
Nikolay Lopin <lopinopulos@gmail.com>
2013-11-29 10:34:35 +0600

    Страничка с финансами

--- app/controllers/admin/finances_controller.rb
+++ app/controllers/admin/finances_controller.rb
@@ -3,7 +3,8 @@ class Admin::FinancesController < Admin::AdminController
   authorize_resource class: false
 
   def index
-    @incomes = FinanceStat.year_income(params[:date] ? Date.parse(params[:date]) : Date.today )
-    @earnings = FinanceStat.year_earnings(params[:date] ? Date.parse(params[:date]) : Date.today )
+    date = params[:year] ? Date.parse(params[:year]) : Date.today
+    @incomes = FinanceStat.year_income(date)
+    @earnings = FinanceStat.year_earnings(date)
   end
 end
